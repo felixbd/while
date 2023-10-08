d9e9002d09f701e5bea34db05563a4bd5c733b3d
Felix Benedikt Drees <fdrees@uni-bremen.de>
2023-10-08 22:49:15 +0200

    Next time someone asks you how to fix an infinite loop, remember this commit

--- app/Main.hs
+++ app/Main.hs
@@ -8,4 +8,7 @@ import WhileParser (tokenize)
 main :: IO ()
 main = do
   someFunc
-  print $ tokenize "x_1 := 10; while x != 0 do x := x - 1 end"
+  print $ tokenize [(1, "x_1 := 10;\n"),
+                    (2, "while x != 0 do\n"),
+                    (3, "    x := x - 1\n"),
+                    (4, "end")]
--- src/WhileParser.hs
+++ src/WhileParser.hs
@@ -7,10 +7,11 @@ module WhileParser
   ,tokenize
   ) where
 
-
+import System.IO
+import Control.Monad
+import Control.Applicative
 import Data.Char (isDigit, isAlpha)
 import Data.List (stripPrefix)
-import Control.Applicative
 
 
 {-|
@@ -30,10 +31,13 @@ data Token = VarToken String  -- e.g. x_1 (has to start with letter - can contai
            | EndToken         -- end
            deriving (Show, Eq)
 
-
 type LineNum = Int
-type ColNum = Int
-type MetaToken = (LineNum, ColNum, Token)
+type ColumnNum = Int
+data MetaToken = MetaToken { getLineNumber :: LineNum,
+                             getColumnNumber :: ColumnNum,
+                             getToken :: Token
+                           } deriving (Show, Eq)
+
 
 -- TODO(felix): upgrade current verwion to handel errors better
 --  newtype TokenParser a = TokenParser {
@@ -41,12 +45,6 @@ type MetaToken = (LineNum, ColNum, Token)
 --    }
 newtype Parser a = Parser { runParser :: [Token] -> Maybe ([Token], a) }
 
-{-
-instance Functor Parser where
-  fmap func (Parser tokenList) = Parser helper
-    where
-      helper
--}
 
 instance Functor Parser where
   -- fmap = (<$>)
@@ -68,7 +66,7 @@ instance Applicative Parser where
 
 instance Alternative Parser where
   empty = Parser $ \_ -> Nothing
-  (Parser pl) <|> (Parser pr) = Parser $ \x -> pl x <|> pr x
+  (Parser pl) <|> (Parser pr) = Parser $ \x -> pl x <|> pr x  -- since Maybe implements Alternative ...
 
 
 
@@ -94,26 +92,30 @@ data Expression = Constant Int                    --  Integer constant
 Function for converting a given String (while programm sorce code)
 to a list of while programm tokens.
 -}
-tokenize :: String -> [Token]
-tokenize [] = []
-tokenize cx@(c:cs)
-  | c `elem` " \n\t" = tokenize cs -- Ignoriere Leerzeichen und Zeilenumbrüche
+tokenize :: [(LineNum, String)] -> [MetaToken]
+tokenize = concat . map (uncurry (flip tokenizeHelper))
+
+-- TODO(felix): add column number
+tokenizeHelper :: String -> LineNum -> [MetaToken]
+tokenizeHelper [] _ = []
+tokenizeHelper cx@(c:cs) ln
+  | c `elem` " \n\t" = tokenizeHelper cs ln -- Ignoriere Leerzeichen und Zeilenumbrüche
   | isAlpha c = uncurry keywordToken $ span (\x -> isAlpha x || isDigit x || x == '_') cx
-  | isDigit c = uncurry (\x y -> ConstToken (read x) : tokenize y) $ span isDigit cx
-  | c == ':', Just rest <- stripPrefix "=" cs = AssignToken : tokenize rest
-  | c == ';' = SemicolonToken : tokenize cs
-  | c == '!', Just rest <- stripPrefix "=" cs = NotEqualToken : tokenize rest
-  | c == '+' = PlusToken : tokenize cs
-  | c == '-' = MinusToken : tokenize cs
+  | isDigit c = uncurry (\x y -> (MetaToken ln 0 (ConstToken (read x))) : tokenizeHelper y ln) $ span isDigit cx
+  | c == ':', Just rest <- stripPrefix "=" cs = MetaToken ln 0 AssignToken : tokenizeHelper rest ln
+  | c == ';' = MetaToken ln 0 SemicolonToken : tokenizeHelper cs ln
+  | c == '!', Just rest <- stripPrefix "=" cs = MetaToken ln 0 NotEqualToken : tokenizeHelper rest ln
+  | c == '+' = MetaToken ln 0 PlusToken : tokenizeHelper cs ln
+  | c == '-' = MetaToken ln 0 MinusToken : tokenizeHelper cs ln
   | otherwise = error $ "Ungültiges Zeichen: " ++ [c]
   where
-    keywordToken :: String -> String -> [Token]
+    keywordToken :: String -> String -> [MetaToken]
     keywordToken token rest = case token of
-      "loop"  -> LoopToken : tokenize rest
-      "while" -> WhileToken : tokenize rest
-      "do"    -> DoToken : tokenize rest
-      "end"   -> EndToken : tokenize rest
-      _       -> VarToken token : tokenize rest
+      "loop"  -> MetaToken ln 0 LoopToken : tokenizeHelper rest ln
+      "while" -> MetaToken ln 0 WhileToken : tokenizeHelper rest ln
+      "do"    -> MetaToken ln 0 DoToken : tokenizeHelper rest ln
+      "end"   -> MetaToken ln 0 EndToken : tokenizeHelper rest ln
+      _       -> MetaToken ln 0 (VarToken token) : tokenizeHelper rest ln
 
 
 tokenParser :: Token -> Parser Token
@@ -128,3 +130,8 @@ tokenParser t = Parser helper
 
 tokenListParser :: [Token] -> Parser [Token]
 tokenListParser = sequenceA . map tokenParser
+
+
+-- Read the content of a file and return it as a list of its lines with numbering
+readFileContents :: FilePath -> IO [(LineNum, String)]
+readFileContents filePath = readFile filePath >>= \c -> return $ zip [1..] (lines c)
