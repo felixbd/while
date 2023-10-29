36d9bc32693ad148848e51cecf18580dcd1d0c17
Felix Benedikt Drees <fdrees@uni-bremen.de>
2023-10-29 03:14:26 +0100

    add test setup

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,21 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+-- module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+-- import Sum
+
+summe :: Integer -> Integer
+summe n = if n <= 0 then 0 else div (n * (n + 1)) 2
+
+
+main = defaultMain $ testGroup "0. Übungsblatt" [exercise0]
+
+exercise0 = testGroup "Summe" [
+    testCase "summe 0" $ summe 0 @?= 0
+  , testCase "summe 5" $ summe 5 @?= 15
+  ]
+
+
+-- main :: IO ()
+-- main = putStrLn "Test suite not yet implemented"
